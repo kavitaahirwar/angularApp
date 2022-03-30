91e9379312d06d4eeb559434db0a564b0a439f58
kavitaahirwar <kavitaahirwar0206@gmail.com>
2022-03-30 11:11:04 +0530

    kavita

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/admin", adminRouter);
-app.use("/user", userRouter);
+// app.use("/user", userRouter);
 
 app.listen(port, () => {
     console.log("server is running..");
